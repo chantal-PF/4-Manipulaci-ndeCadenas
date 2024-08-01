ccfb047bcb08102897f6729aa8630bbf5f05ed3b
Chantal Peñafiel <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-08-01 01:21:43 -0500

    Ejercicios-4

---
+++ 2-Reemplazar.py
@@ -0,0 +1,8 @@
+# Objetivo: Aprender a trabajar con cadenas de caracteres.
+# Instrucciones: Escribe una función que tome una cadena de texto y realice las siguientes operaciones:
+# Reemplace todas las ocurrencias de una letra específica con otra
+# Imprima la cadena modificada y la lista de palabras.
+
+texto = "Hola soy chanty jijiji"
+texto1 = texto.replace("jijiji" , "jojojo")
+print(texto1)
